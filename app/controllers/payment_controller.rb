f8382f771c3aa8defe9c424c16ab60a7bef1644c
thomas <victoria.thomas-pro@hotmail.fr>
2017-02-21 19:53:54 +0100

    feat(gem): add httparty

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,6 +1,11 @@
 class PaymentController < ApplicationController
 
   def yolo
+
+	response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+
+	puts response
+
   	@yolo = "The YOLO is real"
   end
 
